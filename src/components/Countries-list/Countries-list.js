869806e365dacb2c1a9b8f861aba6ae2a9e95d92
Diana <dianastanciu20@gmail.com>
2019-12-06 19:42:38 +0200

    Country Card base styling

--- src/components/Countries-list/Countries-list.js
+++ src/components/Countries-list/Countries-list.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
 import CountryCard from '../Country-card/Country-card';
 import Sort from '../Sort/Sort';
+import styled from 'styled-components';
+
+const CardList = styled.div`
+    display: flex;
+    flex-flow: row wrap;
+`;
 
 class CountriesList extends Component {
     render() {
         return (
             <div className="container">
                 <Sort />
-                {this.props.countries.map((country) => (
-                    <CountryCard
-                        key={country.alpha3Code}
-                        id={country.alpha3Code}
-                        name={country.name}
-                    />
-                ))}
+                <CardList>
+                    {this.props.countries.map((country) => (
+                        <CountryCard
+                            population={country.population}
+                            region={country.region}
+                            capital={country.capital}
+                            flag={country.flag}
+                            key={country.alpha3Code}
+                            id={country.alpha3Code}
+                            name={country.name}
+                        />
+                    ))}
+                </CardList>
             </div>
         )
     }
--- src/components/Country-card/Country-card.js
+++ src/components/Country-card/Country-card.js
@@ -3,8 +3,32 @@ import styled from 'styled-components';
 import { Link } from "react-router-dom";
 
 const Card = styled.div`
-    border: 1px dotted rgba(0,0,0,.3);
     margin-bottom: 10px;
+    width: 25%;
+    text-align: center;
+    
+    a {
+        background: #fff;
+        margin: 10%;
+        display: block;
+        border-radius: 5px;
+        text-decoration: none;
+    }
+    
+    img {
+        width: 100%;
+    }
+`;
+
+const CardInfo = styled.div`
+    text-align: left;
+    color: #000;
+    padding: 0 20px 20px 20px;
+    font-size: 14px;
+    
+    h2 {
+        font-size: 18px;
+    }
 `;
 
 class CountryCard extends Component {
@@ -12,7 +36,13 @@ class CountryCard extends Component {
         return(
             <Card>
                 <Link to={`country-details/${this.props.id}`}>
-                    <button>{this.props.name}</button>
+                    <img src={this.props.flag} alt=""/>
+                    <CardInfo>
+                        <h2>{this.props.name}</h2>
+                        <p><strong>Population</strong>: {this.props.population}</p>
+                        <p><strong>Region</strong>: {this.props.region}</p>
+                        <p><strong>Capital</strong>: {this.props.capital}</p>
+                    </CardInfo>
                 </Link>
             </Card>
         )
